cbd12575786d47e104dcda91e3330fbffdc6e7a5
Andrew <overfitted@gmail.com>
2021-05-20 20:54:54 +0800

    cpp, kattis

---
+++ Kattis/moscowdream.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  int a,b,c,n; 
+  scanf("%d, %d, %d, %d", &a, &b, &c, &n);
+  if((a>=1) && (b >= 1) && (c >= 1) && (a+b+c>=n)){
+    printf("YES\n");
+  } else {
+    printf("NO\n");
+  }
+  return 0;
+}
