0bfc393986f10ba42f03a33ac7ce5cf5cf2449b6
Andrew <overfitted@gmail.com>
2021-12-18 20:44:32 +0800

    18/12 library

---
+++ Kattis/success/sumsquareddigits/sumsquareddigits.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define REP(i,a,b) for (int i = a; i < b; i++)
+#define ITER(x) for (int i = 0; i < x; i++)
+#define elif else if
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+vector<string> split(string s, string delimiter){
+    vector<string> ans;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+
+        ans.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    ans.push_back(s);
+    return ans;
+}
+
+void solve() {
+    int p;
+    cin>>p;
+    ITER(p){
+      int k,b,n,x;
+      ll ans =0;
+      cin>>k>>b>>n;
+      int degree=0;
+      x=n;
+      while(x>0){
+        x=x/b;
+        degree++;
+      }
+      for(int i=degree-1;i>=0;i--){
+        ans+=pow(n/(int)pow(b,i),2);
+        n=n%(int)pow(b,i);
+      }
+      cout<<k<<" "<<ans<<"\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+}
